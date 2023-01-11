6d937116a71270ca76170f08a90a0f59f5adab0b
Benyuan Cao <cby204@gmail.com>
2023-01-10 22:25:35 -0500

    add smartsheet api

--- server/server.js
+++ server/server.js
@@ -27,3 +27,40 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on the port::${port}`)
 })
+
+// Initialize the client
+var client = require('smartsheet')
+var smartsheet = client.createClient({
+  accessToken: 'JKlMNOpQ12RStUVwxYZAbcde3F5g6hijklM789',
+  logLevel: 'info',
+})
+
+// The `smartsheet` variable now contains access to all of the APIs
+
+// Set queryParameters for `include` and pagination
+var options = {
+  queryParameters: {
+    include: 'attachments',
+    includeAll: true,
+  },
+}
+
+// List all sheets
+smartsheet.sheets
+  .listSheets(options)
+  .then(function (result) {
+    var sheetId = result.data[0].id // Choose the first sheet
+
+    // Load one sheet
+    smartsheet.sheets
+      .getSheet({ id: sheetId })
+      .then(function (sheetInfo) {
+        console.log(sheetInfo)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
